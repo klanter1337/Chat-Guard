4d35e8efde3256eb95bfb363fc74bd2c1af1b5de
Klanter <68739161+KlanterV@users.noreply.github.com>
2021-05-10 22:16:36 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ const config = {
     MongoDB_ConnectURL: '',
     //------------BotSettings-----------//
     Prefix: '',
+    BotsOwner: [""],
     Custom_Status_Text: '',
     Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
     Custom_Status: 'dnd', // => dnd / idle / online / invisible
