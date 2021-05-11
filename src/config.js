e3cec4c97f4facc41f731fd545fdb22c1ce347a6
Klanter <68739161+KlanterV@users.noreply.github.com>
2021-05-11 20:45:39 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = { 
 //------------Connections-----------//
-    Client_Token: 'ODAzMTk0Nzc1NDQ3NTM1NjU3.YA6PbQ.1i7i0aM0teX3nTv62oUyXAid-K8',
-    MongoDB_ConnectURL: 'mongodb+srv://test:test123123@cluster0.m4zsf.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    Client_Token: '',
+    MongoDB_ConnectURL: '',
     //------------BotSettings-----------//
-    Prefix: '.',
-    BotOwners: ["786601295737651201"],
-    Custom_Status_Text: 'sa',
+    Prefix: '',
+    BotOwners: [""],
+    Custom_Status_Text: 'hey',
     Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
     Custom_Status: 'dnd', // => dnd / idle / online / invisible
-    VoiceChannelID: '820734704084975687'
+    VoiceChannelID: ''
 };
   
 module.exports = config;
