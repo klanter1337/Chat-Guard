cd1e366f3e1367fe73f652ec8739e33323ab648c
Klanter <68739161+KlanterV@users.noreply.github.com>
2021-05-13 18:58:05 +0300

    Update events.js

--- src/events.js
+++ src/events.js
@@ -22,6 +22,13 @@ client.on('message', async (message) => {
         if (message && message.deletable) message.delete({ timeout: 0110 }).catch(() => {});
         return await Punish(message, 'MassPingGuard', Embed);
     }
+
+    if(await Spam(message) === true ) {
+        if(Database.SpamGuard === false || Database.SpamGuard === null || !Database.SpamGuard) return;
+        if (message && message.deletable) message.delete({ timeout: 0130 }).catch(() => {});
+        return await Punish(message, 'SpamGuard', Embed);
+    }
+    
     let InviteGuardReg = /(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i;  
     if (InviteGuardReg.test(message.content)){
         if(Database.InviteGuard === false || Database.InviteGuard === null || !Database.InviteGuard) return;
@@ -45,13 +52,45 @@ client.on('message', async (message) => {
         if (message && message.deletable) message.delete({ timeout: 0150 }).catch(() => {});
         return message.channel.send(Embed.setDescription('<@'+message.author.id+'>, Filtrelenmiş kelime içeren mesajlar kullanman yasak !')).then(x => x.delete({timeout: 3000})).catch(() => {});
     }
-    if(await Spam(message) === true ) {
-        if(Database.SpamGuard === false || Database.SpamGuard === null || !Database.SpamGuard) return;
-        if (message && message.deletable) message.delete({ timeout: 0130 }).catch(() => {});
-        return await Punish(message, 'SpamGuard', Embed);
-    }
 });
 
+client.on('messageUpdate', async (oldMessage, newMessage) => {
+    if(!newMessage.guild || newMessage.author.bot || newMessage.member.hasPermission(8) || newMessage.author.id === newMessage.guild.ownerID) return;
+     const Database = await db.findOne({ ServerID: newMessage.guild.id });
+     if(!Database || await WhiteList(newMessage) === true) return;
+     const Embed = new MessageEmbed().setColor(RandomColor(true)).setTimestamp().setAuthor(newMessage.guild.name, newMessage.guild.iconURL({ dynamic: true }));
+ 
+     if(message.content.length > '500') { 
+         if(Database.CharacterLimit === false || Database.CharacterLimit === null || !Database.CharacterLimit) return;
+         if (newMessage && newMessage.deletable) newMessage.delete({ timeout: 0100 }).catch(() => {});
+         return await Punish(newMessage, 'CharacterLimit', Embed);
+     }
+
+     let InviteGuardReg = /(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i;  
+     if (InviteGuardReg.test(newMessage.content)){
+         if(Database.InviteGuard === false || Database.InviteGuard === null || !Database.InviteGuard) return;
+         if (newMessage && newMessage.deletable) newMessage.delete({ timeout: 0120 }).catch(() => {});
+         return await Punish(newMessage, 'InviteGuard', Embed);
+     }
+    
+     let LinkGuardReg = /(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/gi;
+     if (LinkGuardReg.test(newMessage.content)){
+         if(Database.LinkGuard === false || Database.LinkGuard === null || !Database.LinkGuard) return;
+         if (newMessage && newMessage.deletable) newMessage.delete({ timeout: 0130 }).catch(() => {});
+         return newMessage.channel.send(Embed.setDescription('<@'+newMessage.author.id+'>, Link içeren mesajlar kullanman yasak !')).then(x => x.delete({timeout: 3000})).catch(() => {});
+     }
+     if(BadWord(newMessage.content) === true) { 
+         if(Database.BadWordGuard === false || Database.BadWordGuard === null || !Database.BadWordGuard) return;
+         if (newMessage && newMessage.deletable) newMessage.delete({ timeout: 0140 }).catch(() => {});
+         return newMessage.channel.send(Embed.setDescription('<@'+newMessage.author.id+'>, Küfür içeren mesajlar kullanman yasak !')).then(x => x.delete({timeout: 3000})).catch(() => {});
+     }
+    
+     if (Database && Database.FiltredWords.includes(newMessage.content.toLowerCase()) === true) {
+         if (newMessage && newMessage.deletable) newMessage.delete({ timeout: 0150 }).catch(() => {});
+         return newMessage.channel.send(Embed.setDescription('<@'+newMessage.author.id+'>, Filtrelenmiş kelime içeren mesajlar kullanman yasak !')).then(x => x.delete({timeout: 3000})).catch(() => {});
+     }
+ });
+ 
 client.on('guildMemberAdd', async (member) => {
     const Database = await db.findOne({ ServerID: member.guild.id });
     
